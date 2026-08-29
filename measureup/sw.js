c86ae74405375ca4dac9ffd8296d3bc3d584ee90
motbuchanan <motbuchanan@gmail.com>
2026-08-28 22:31:38 -0400

    Add files via upload

--- measureup/sw.js
+++ measureup/sw.js
@@ -1,4 +1,4 @@
-const CACHE='measureup-v0-55';
+const CACHE='measureup-v0-57';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
